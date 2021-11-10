7994366226a6a9da29bde4e7e3e9056fb7761023
CapacitorA10 <53959052+CapacitorA10@users.noreply.github.com>
2021-11-10 23:43:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 ##
-import yfinance as yf
-
+import DataTools as DTs
 import torch
 import torch.nn.init
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset
-import torchvision.transforms as transforms
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
 import pandas as pd
 from torch.utils.tensorboard import SummaryWriter
 writer = SummaryWriter()
@@ -18,60 +15,31 @@ torch.cuda.is_available()
 ## data import
 start_date = '2003-01-01'
 end_date = '2021-11-09'
-dateRange = pd.date_range(start=start_date, end=end_date)
-
-stock_spy = yf.download('SPY',start_date,end_date)
-stock_nsdq = yf.download('NQ=F',start_date,end_date)
-stock_tlt = yf.download('TLT',start_date,end_date)
-stock_gold = yf.download('GC=F',start_date,end_date)
-stock_oil = yf.download('CL=F',start_date,end_date)
-
-stock_spy["Adj Close"].plot()
-stock_tlt["Adj Close"].plot()
-stock_gold["Adj Close"].plot()
-stock_oil["Adj Close"].plot()
-stock_nsdq["Adj Close"].plot()
-plt.show()
+stocks = DTs.data_import(start_date, end_date)
 
 ## dataset 설정
 input_t = 30 # 입력데이터 period
 output_t = 10 # 출력데이터 period
 
 class stockdataset(Dataset):
-    def __init__(self):
-        self.data_spy = stock_spy
-        self.data_tlt = stock_tlt
-        self.data_oil = stock_oil
-        self.data_gold = stock_gold
-        self.data_nsdq = stock_nsdq
+    def __init__(self, stock):
+        self.data_spy = stock['spy']
+        self.data_nsdq = stock['nsdq']
+        self.data_tlt = stock['tlt']
+        self.data_oil = stock['oil']
+        self.data_gold = stock['gold']
 
     def __len__(self):
         return len(self.data_spy)
 
     def data_pre_process(self):
-        def make_stationary(data):
-            for i in range(1, len(data)):
-                # 볼륨값을 제외한 나머지는 맨 뒤에서부터 앞에값의 차이로 구한다
-                temp = data.iloc[-i]  # 맨뒤
-                temp2 = data.iloc[-(i + 1)]  # 그 전날
-                newtemp = ((temp[0:6] / temp2[0:5]) - 1)*100 # 당일/전날 하여 전날대비상승률 계산, 1을 빼서 정확한 %계산
-                newtemp['Volume'] = temp['Volume']  # 볼륨값 유지
-                data.iloc[-i] = newtemp
-            # Volume 평준화 시행
-            data['Volume'] = data['Volume'] / data['Volume'].max()
-
-
-
-        make_stationary(self.data_spy)
-        make_stationary(self.data_tlt)
-        #make_stationary(self.data_oil) #oil 선물은 그냥 사용? -> 2020 4월에 음전한 경력이 있어 상승,하락률 계산도 어려움
-        make_stationary(self.data_gold)
-        make_stationary(self.data_nsdq)
+        DTs.to_percentage(self.data_spy)
+        DTs.to_percentage(self.data_tlt)
+        DTs.to_percentage(self.data_gold)
+        DTs.to_percentage(self.data_nsdq)
         # oil은 Volume값 최적화만 수행
         self.data_oil['Volume'] = self.data_oil['Volume'] / self.data_oil['Volume'].max()
-
         # 데이터 숫자 맞추기 #
-        # reindex를 사용한다? -> 가장 많은 놈 기준으로.. 현재 spy, tlt가 최다
         # reindex에서 gold, nsdq은 변화율 기준이니 값 0을 입력해주면 됨
         # 하지만 oil은 절대값 기준이므로 이전값을 대입하면 됨
         self.data_gold = self.data_gold.reindex(self.data_spy.index, fill_value=0)
@@ -107,15 +75,17 @@ class stockdataset(Dataset):
         return  date, input_dic, output_dic
 
 ## data 전처리 및 show
-dataset = stockdataset()
+dataset = stockdataset(stocks)
 dataset.data_pre_process()
-
+'''
 dataset.data_spy["Adj Close"][1:].plot()
 dataset.data_tlt["Adj Close"][1:].plot()
 dataset.data_gold["Adj Close"][1:].plot()
 dataset.data_oil["Adj Close"][1:].plot()
 dataset.data_nsdq["Adj Close"][1:].plot()
 plt.show()
+''' # 데이터 plot
+
 
 ## MODEL 3COMBI DEFINE
 class comb_model1(torch.nn.Module):
@@ -246,23 +216,9 @@ for epoch in range(max_epoch):
 ################################################## 여기서부터 검증 ######################################################
 ## data import
 
-
 start_date = '2019-08-01'
 end_date = '2021-11-09'
-dateRange = pd.date_range(start=start_date, end=end_date)
-
-vstock_spy = yf.download('SPY',start_date,end_date)
-vstock_nsdq = yf.download('NQ=F',start_date,end_date)
-vstock_tlt = yf.download('TLT',start_date,end_date)
-vstock_gold = yf.download('GC=F',start_date,end_date)
-vstock_oil = yf.download('CL=F',start_date,end_date)
-
-vstock_spy["Adj Close"].plot()
-vstock_tlt["Adj Close"].plot()
-vstock_gold["Adj Close"].plot()
-vstock_oil["Adj Close"].plot()
-vstock_nsdq["Adj Close"].plot()
-plt.show()
+vstock_spy, vstock_nsdq, vstock_tlt, vstock_gold, vstock_oil = DTs.data_import(start_date, end_date)
 
 ## dataset 설정 및 전처리
 
