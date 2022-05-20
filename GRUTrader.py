73d911cd9cd6eb9a03d21f8380a3cb01270b0773
CapacitorA10 <53959052+CapacitorA10@users.noreply.github.com>
2022-05-20 10:53:21 +0900

    Add files via upload

---
+++ GRUTrader.py
@@ -0,0 +1,29 @@
+##
+import ai_trader.DataTools as DTs
+import torch
+import torch.nn.init
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from torch.utils.tensorboard import SummaryWriter
+
+writer = SummaryWriter()
+device = 'cuda'
+input_t = int(input("input length? "))# 입력데이터 period 100 - 70 - 30 - 15 - 6 등...
+period = int(input("period term? "))#입력-출력간 기간
+output_t = 1 # 출력데이터 길이(채권, 나스닥, 금)
+bSize = 32   # 배치 사이즈
+torch.cuda.is_available()
+## Data import
+
+stocks = DTs.data_import('1995-01-01', '2025-01-01')
+periodDiff_stocks = DTs.data_pre_process_period(stocks, period=period)
+DTs.data_pre_process_(stocks)
+stock_train_x = DTs.split(stocks, '2001-01-01', '2019-06-30')
+stock_train_y = DTs.split(periodDiff_stocks, '2001-01-01', '2019-06-30')
+stock_test_x = DTs.split(stocks, '2019-07-01', '2025-01-01')
+stock_test_y = DTs.split(periodDiff_stocks, '2019-07-01', '2025-01-01')
+##
+
