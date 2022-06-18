ac875e6a9cdb611dd12eaf2643b6e60d23c94e83
CapacitorA10 <53959052+CapacitorA10@users.noreply.github.com>
2022-06-18 12:26:27 +0900

    Update main.py

--- GRUver/main.py
+++ GRUver/main.py
@@ -14,7 +14,7 @@ torch.cuda.is_available()
 
 time_step = 20
 time_term = 15 #time step 막날로부터 xx일 후 예측
-bSize = 1   # 배치 사이즈
+bSize = 2  # 배치 사이즈
 learning_rate = 0.0001
 
 stocks = DTs.data_import('2000-09-01', '2025-01-01') #item변수 전달 안하면, 기본 3개 나스닥 채권 금만 return
@@ -37,7 +37,7 @@ X_train = torch.FloatTensor(np.asarray(X_train)).to(device)
 X_test = torch.FloatTensor(np.asarray(X_test)).to(device)
 
 train_loader = torch.utils.data.DataLoader(dataset=X_train, batch_size=bSize, shuffle=True)
-test_loader  = torch.utils.data.DataLoader(dataset=X_test, batch_size=bSize, shuffle=True)
+test_loader  = torch.utils.data.DataLoader(dataset=X_test, batch_size=1, shuffle=False)
 #print(f"Shape:\n Xtrain:{X_train.shape} Ytrain:{Y_train.shape}\n Xtest:{X_test.shape} Ytest:{Y_test.shape}")
 
 
@@ -45,7 +45,7 @@ test_loader  = torch.utils.data.DataLoader(dataset=X_test, batch_size=bSize, shu
 
 
 input_size  = X_train.shape[-1]
-gru_out_size = 12
+gru_out_size = 12 #(==hidden size)
 num_layers  = 1
 num_classes = X_train.shape[-1]//2 # 출력은 나스닥 채권 금 close.. (+달러인덱스도?
 
@@ -58,23 +58,48 @@ writer.add_graph(MODEL, X_test)
 
 loss_graph = [] # 그래프 그릴 목적인 loss.
 n = len(train_loader)
+num_epochs = 20
+##
 step = 0
-num_epochs = 5
-for epoch in range(num_epochs):
-    running_loss = 0.0
+v_step = 0
 
+for epoch in range(num_epochs):
+    avg_train = 0
     for data in train_loader:
 
-        out = MODEL(data[:,:-1,:])  # 모델에 넣고,
-        loss = criterion(out, data[:,-1,0:3]) # output 가지고 loss 구하고,
+        MODEL.train()
+        optimizer.zero_grad()
+
+        out = MODEL(data[:,:-1,:])
+        loss = criterion(out, data[:,-1,0:3])
+        loss.backward()
+        optimizer.step()
+
         writer.add_scalar("Loss/train", loss.sum() / (bSize), step)
-        optimizer.zero_grad() #
-        loss.backward() # loss가 최소가 되게하는
-        optimizer.step() # 가중치 업데이트 해주고,
-        running_loss += loss.item() # 한 배치의 loss 더해주고,
         step+=1
+        avg_train += loss
+        #verification
 
-    loss_graph.append(running_loss / n) # 한 epoch에 모든 배치들에 대한 평균 loss 리스트에 담고,
-    print('[epoch: %d] loss: %.4f'%(epoch, running_loss))
+        if step % 2000 == 0:
+            avg_test = 0
+            with torch.no_grad():
+                MODEL.eval()
+                for data in test_loader:
+                    out = MODEL(data[:, :-1, :])
+                    loss = criterion(out, data[:, -1, 0:3])
+                    avg_test += loss
+                    writer.add_scalar("Loss/test", loss.sum() / (bSize), v_step)
+                    writer.add_scalar("REAL/GOLD", data.squeeze()[-1, 0], v_step)
+                    writer.add_scalar("REAL/NSDQ", data.squeeze()[-1, 1], v_step)
+                    writer.add_scalar("REAL/TRES", data.squeeze()[-1, 2], v_step)
+                    writer.add_scalar("PREDICTED/GOLD", out.squeeze()[0], v_step)
+                    writer.add_scalar("PREDICTED/NSDQ", out.squeeze()[1], v_step)
+                    writer.add_scalar("PREDICTED/TRES", out.squeeze()[2], v_step)
+                    v_step+=1
+                print(f"TEST:  epoch/step:{epoch}/{step},  avg loss:{avg_test/X_test.shape[0]}")
 
+
+    print(f"TRAIN: epoch:{epoch},        avg loss:{avg_train/X_train.shape[0]}\n")
 ##
+experiments = torch.FloatTensor(np.asarray(stocks_1day_change.iloc[-20:, :])).unsqueeze(0)
+predicted = MODEL(experiments.to(device))
