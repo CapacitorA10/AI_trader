ec07ad11bf46bfc52fdc1ad4b5bb0e4e1ecc53f8
CapacitorA10 <53959052+CapacitorA10@users.noreply.github.com>
2022-07-26 20:10:22 +0900

    Update model.py

--- GRUver/model.py
+++ GRUver/model.py
@@ -2,17 +2,17 @@ import torch
 import torch.nn as nn
 
 class GRU(nn.Module):
-    def __init__(self, num_classes, input_size, gru_out_size, num_layers, seq_length):
+    def __init__(self, num_classes, input_size, hidden_size, num_layers, seq_length):
         super(GRU, self).__init__()
         self.num_classes = num_classes
         self.num_layers  = num_layers
         self.input_size  = input_size
-        self.hidden_size = gru_out_size
+        self.hidden_size = hidden_size
         self.seq_length  = seq_length
 
-        self.gru = nn.GRU(input_size=input_size, hidden_size=gru_out_size,
+        self.gru = nn.GRU(input_size=input_size, hidden_size=hidden_size,
                           num_layers=num_layers, batch_first=True)
-        self.fc_1 = nn.Linear(gru_out_size, 64)
+        self.fc_1 = nn.Linear(hidden_size, 64)
         self.fc = nn.Linear(64, num_classes)
         self.relu = nn.ReLU()
         self.tanh = nn.Tanh()
@@ -24,6 +24,7 @@ class GRU(nn.Module):
 
         #new
         out, (h_n) = self.gru(x, h)
+        #print(out.shape)
         out = out[:,-1,:] # LAST OUTPUT 추출
         out = self.tanh(out.view(-1,self.hidden_size))
         out = self.fc_1(out)
