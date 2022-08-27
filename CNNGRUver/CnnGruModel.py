3c1c19a5e5e47c4e836dc1b637b786a37fa58646
CapacitorA10 <53959052+CapacitorA10@users.noreply.github.com>
2022-08-27 20:59:26 +0900

    Now, can use CNNGRU, but Check inputs
    dd

--- CNNGRUver/CnnGruModel.py
+++ CNNGRUver/CnnGruModel.py
@@ -1,92 +1,124 @@
-import torch
+##
+import DataTools as DTs
+from CnnGruModel import CNNGRU
+import torch.nn.init
+from torch.utils.data import DataLoader
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
+import pandas as pd
 
-## MODEL DEFINE
-class CNNGRU(torch.nn.Module):
+writer = SummaryWriter()
+device = 'cpu'
+torch.cuda.is_available()
 
-    def __init__(self, num_classes, input_size, hidden_size, num_layers, seq_length):
-        super(CNNGRU, self).__init__()
-        self.num_classes = num_classes  # output Class 수
-        self.num_layers = num_layers  # GRU layer 수
-        self.input_size = input_size  # input feature class 수
-        self.hidden_size = hidden_size
-        self.seq_length = seq_length
-        cnn_feature = 64
+time_step = 20
+time_term = 5  # time step 막날로부터 xx일 후 예측
+"""if time_term > time_step:
+    print("ERR: time step MUST bigger than time term")
+    exit()"""
+bSize = 2  # 배치 사이즈
+learning_rate = 0.0001
+num_epochs = 100
 
-        self.cnn1 = torch.nn.Sequential(
-            torch.nn.Conv1d(2, cnn_feature, kernel_size=5, padding=2, bias=True),
-            torch.nn.BatchNorm1d(cnn_feature),
-            torch.nn.MaxPool1d(2, stride=2),
-            torch.nn.ReLU(),
-            torch.nn.Conv1d(cnn_feature, cnn_feature, kernel_size=3, padding=1, bias=True),
-            torch.nn.BatchNorm1d(cnn_feature),
-            # torch.nn.MaxPool1d(2, stride=2),
-            torch.nn.ReLU()
-        )
-        self.cnn2 = torch.nn.Sequential(
-            torch.nn.Conv1d(2, cnn_feature, kernel_size=5, padding=2, bias=True),
-            torch.nn.BatchNorm1d(cnn_feature),
-            torch.nn.MaxPool1d(2, stride=2),
-            torch.nn.ReLU(),
-            torch.nn.Conv1d(cnn_feature, cnn_feature, kernel_size=3, padding=1, bias=True),
-            torch.nn.BatchNorm1d(cnn_feature),
-            # torch.nn.MaxPool1d(2, stride=2),
-            torch.nn.ReLU()
-        )
-        self.cnn3 = torch.nn.Sequential(
-            torch.nn.Conv1d(2, cnn_feature, kernel_size=5, padding=2, bias=True),
-            torch.nn.BatchNorm1d(cnn_feature),
-            torch.nn.MaxPool1d(2, stride=2),
-            torch.nn.ReLU(),
-            torch.nn.Conv1d(cnn_feature, cnn_feature, kernel_size=3, padding=1, bias=True),
-            torch.nn.BatchNorm1d(cnn_feature),
-            # torch.nn.MaxPool1d(2, stride=2),
-            torch.nn.ReLU()
-        )
-        comb_feature = cnn_feature * (input_size // 2)
-        self.cnn_comb = torch.nn.Sequential(
-            torch.nn.Conv1d(comb_feature, comb_feature, kernel_size=5, padding=2, bias=True),
-            torch.nn.BatchNorm1d(comb_feature),
-            torch.nn.ReLU(),
-            torch.nn.Conv1d(comb_feature, comb_feature, kernel_size=3, padding=1, bias=True),
-            torch.nn.BatchNorm1d(comb_feature),
-            torch.nn.ReLU()
-        )
+stocks = DTs.data_import('2000-09-01', '2025-01-01')  # item변수 전달 안하면, 기본 3개 나스닥 채권 금만 return
+#stocks = pd.read_csv('data.csv',header=[0,1],index_col=0)
+stocks_1day_change = DTs.pct_change_except_bond(stocks)
+stocks_days_change = DTs.pct_change_except_bond(stocks, time_term)
+# 후가공-표준화
+stocks_1day_change = (stocks_1day_change-stocks_1day_change.mean())/stocks_1day_change.std()
+## data split / XY merge
+split_date = '2020-12-30'
+X_train = DTs.append_time_step(stocks_1day_change.loc['2000-01-01': split_date],
+                               stocks_days_change.loc['2000-01-01': split_date],
+                               time_step, time_term)
 
-        class extract_tensor(torch.nn.Module):
-            def forward(self, x):
-                # Output shape (batch, features, hidden)
-                tensor, _ = x
-                # Reshape shape (batch, hidden)
-                return tensor[:, -1, :]
-        self.gru = torch.nn.Sequential(
-            torch.nn.GRU(input_size=comb_feature, hidden_size=hidden_size,
-                         num_layers=num_layers, batch_first=True),
-            extract_tensor()
-        )
-        self.fc = torch.nn.Sequential(
-            torch.nn.Linear(hidden_size, 32, bias=True),
-            torch.nn.BatchNorm1d(32),
-            torch.nn.ReLU(),
-            torch.nn.Linear(32, num_classes, bias=True)
-        )
+X_test = DTs.append_time_step(stocks_1day_change.loc[split_date: '2025-01-01'],
+                              stocks_days_change.loc[split_date: '2025-01-01'],
+                              time_step, time_term)
 
-    def forward(self, input):
-        feat_size = input.shape[-2]
-        print(f"input: {input.shape}")
-        print(f"cnn input: {input[:, 0::feat_size>>1, :].shape}")
-        c1 = self.cnn1(input[:, 0::feat_size>>1, :])
-        c2 = self.cnn2(input[:, 1::feat_size>>1, :])
-        c3 = self.cnn3(input[:, 2::feat_size>>1, :])
-        print(f"c1: {c1.shape}, c2: {c2.shape}, c3: {c3.shape} ")
-        out = torch.cat((c1, c2, c3), 1)
-        print(f"cat: {out.shape}")
-        out = self.cnn_comb(out).transpose(1,2)
-        print(f"comb: {out.shape}")
-        h_0 = (torch.zeros(self.num_layers, out.size(0), self.hidden_size)).to('cpu')
-        out = self.gru(out, (h_0))
-        print(f"gru out: {out.shape}")
-        out = out.view(out.size(0), -1)  # Flatten them for FC
-        print(f"gru view: {out.shape}")
-        out = self.fc(out)
-        print(f"output: {out.shape}")
-        return out
+X_train = torch.FloatTensor(np.asarray(X_train)).transpose(1,2).to(device)
+X_test = torch.FloatTensor(np.asarray(X_test)).transpose(1,2).to(device)
+
+train_loader = torch.utils.data.DataLoader(dataset=X_train, batch_size=bSize, shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=X_test, batch_size=1, shuffle=False)
+# print(f"Shape:\n Xtrain:{X_train.shape} Ytrain:{Y_train.shape}\n Xtest:{X_test.shape} Ytest:{Y_test.shape}")
+# test가 시작하는 때의 value(그래프 그리기용)
+test_start_idx = stocks.index.get_loc(split_date)
+test_real_start = torch.FloatTensor(stocks.iloc[test_start_idx - time_term,0:3]).to(device)
+
+## START LEARN
+
+
+input_size = X_train.shape[-2]
+gru_out_size = 8  # (==hidden size)
+num_layers = 1
+num_classes = X_train.shape[-2] // 2  # 출력은 나스닥 채권 금 close.. (+달러인덱스도?
+
+MODEL = CNNGRU(num_classes, input_size, gru_out_size, num_layers, time_step).to(device)
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.RMSprop(MODEL.parameters(), lr=learning_rate)
+
+#writer.add_graph(MODEL, X_train)
+#writer.add_graph(MODEL, X_test)
+
+##
+step = 0
+v_step = 0
+
+for epoch in range(num_epochs):
+    avg_train = 0
+    for data in train_loader:
+
+        MODEL.train()
+        optimizer.zero_grad()
+
+        out = MODEL(data[:, :, :-1])
+        loss = criterion(out, data[:,0:3,-1])
+        loss.backward()
+        optimizer.step()
+
+        writer.add_scalar("Loss/train", loss.sum() / bSize, step)
+        step += 1
+        avg_train += loss
+
+        # verification
+        if step % 2000 == 0:
+            avg_test = 0
+            # 변화량 ->실제값 그래프 그리기 위해 값 추출
+            gold = test_real_start.clone()[0]
+            nasdaq = test_real_start.clone()[1]
+            i_step = 0
+            with torch.no_grad():
+                MODEL.eval()
+                for test_data in test_loader:
+                    out = MODEL(test_data[:, :, :-1])
+                    loss = criterion(out, test_data[:,0:3,-1])
+                    avg_test += loss
+                    writer.add_scalar("Loss/test", loss.sum() / bSize, v_step)
+                    writer.add_scalar("REAL/GOLD", test_data.squeeze()[-1, 0], v_step)
+                    writer.add_scalar("REAL/NSDQ", test_data.squeeze()[-1, 1], v_step)
+                    writer.add_scalar("REAL/TRES", test_data.squeeze()[-1, 2], v_step)
+                    writer.add_scalar("PREDICTED/GOLD", out.squeeze()[0], v_step)
+                    writer.add_scalar("PREDICTED/NSDQ", out.squeeze()[1], v_step)
+                    writer.add_scalar("PREDICTED/TRES", out.squeeze()[2], v_step)
+                    gold *= (100 + out.squeeze()[0]) * 0.01
+                    nasdaq *= (100 + out.squeeze()[1]) * 0.01
+                    date_now = test_start_idx + time_term + time_step + i_step
+                    writer.add_scalars("static-GOLD",{"REAL": stocks.iat[date_now, 0],
+                                                      "PRED": gold} , v_step)
+                    writer.add_scalars("static-NSDQ", {"REAL": stocks.iat[date_now, 1],
+                                                       "PRED": nasdaq}, v_step)
+                    writer.add_scalars("static-TRES", {"REAL": stocks.iat[date_now, 2],
+                                                       "PRED": out.squeeze()[2]}, v_step)
+                    i_step += 1
+                    v_step += 1
+                print(f"TEST:  epoch/step: {epoch}/{step},  avg loss: {avg_test / X_test.shape[0]}")
+        torch.cuda.empty_cache()
+    print(f"TRAIN: epoch: {epoch},  avg loss: {avg_train / X_train.shape[0]}\n")
+## 최종 모델로 오늘로부터 3주 보기
+experiments = torch.FloatTensor(np.asarray(stocks_1day_change.iloc[-time_step:, :])).unsqueeze(0)
+predicted = MODEL(experiments.to(device))
+print(predicted)
+##
+##
