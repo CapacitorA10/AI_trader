fa0b27434252e80ed364002e8a247f48fa67cd53
CapacitorA10 <53959052+CapacitorA10@users.noreply.github.com>
2023-01-13 22:32:12 +0900

    Update model.py

--- gpt3/model.py
+++ gpt3/model.py
@@ -9,7 +9,7 @@ class CNN(nn.Module):
     def forward(self, x):
         x = self.conv1(x)   # Conv1d : (N, C, L)
         x = self.relu(x)
-        return x # X: (N, C, L)
+        return x # X: (N, num_filters, L)
 
 # Define the GRU
 class GRU(nn.Module):
@@ -36,82 +36,3 @@ class Model(nn.Module):
         x = x.reshape(x.shape[0],-1)
         x = self.fc(x)
         return x
-'''
-## Create an instance of the model
-input_size = 1
-num_filters = 8
-kernel_size = 3
-hidden_size = 20
-model = Model(input_size=input_size, num_filters=num_filters, kernel_size=kernel_size, hidden_size=hidden_size)
-
-# Generate some dummy input data
-seq_length = 10
-batch_size = 2
-x = torch.randn(batch_size, input_size, seq_length)
-
-# Forward pass the input through the model
-output, _ = model(x)
-
-
-
-
-######
-
-# Load the data
-data = # Load your data here
-
-# Split the data into training and validation sets
-train_data, val_data = # Split the data here
-
-# Define the model hyperparameters
-input_size = 1
-num_filters = 8
-kernel_size = 3
-hidden_size = 8
-
-# Create an instance of the model
-model = Model(input_size=input_size, num_filters=num_filters, kernel_size=kernel_size, hidden_size=hidden_size)
-
-# Define the loss function and optimizer
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters())
-
-# Train the model
-for epoch in range(num_epochs):
-    # Training phase
-    for data in train_data:
-        # Extract the input and target
-        x = data[0]
-        y = data[1]
-
-        # Forward pass
-        output, _ = model(x)
-        loss = criterion(output, y)
-
-        # Backward pass and optimization step
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-    # Validation phase
-    with torch.no_grad():
-        correct = 0
-        total = 0
-        for data in val_data:
-            # Extract the input and target
-            x = data[0]
-            y = data[1]
-
-            # Forward pass
-            output, _ = model(x)
-
-            # Compute the validation accuracy
-            _, predicted = torch.max(output.data, 1)
-            total += y.size(0)
-            correct += (predicted == y).sum().item()
-
-        val_accuracy = 100 * correct / total
-
-    # Print the epoch-wise training and validation accuracies
-    print("Epoch: {} | Train Loss: {:.4f} | Validation Accuracy: {:.2f}%".format(epoch+1, loss.item(), val_accuracy))
-'''
