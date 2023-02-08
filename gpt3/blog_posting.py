deda73ce0d4e05a775c63dda7b01bbd1d96a2fc7
CapacitorA10 <53959052+CapacitorA10@users.noreply.github.com>
2023-02-08 11:48:29 +0900

    Update blog_posting.py

--- gpt3/blog_posting.py
+++ gpt3/blog_posting.py
@@ -8,6 +8,7 @@ krw_usd = fdr.DataReader('FRED:DEXKOUS')
 t10y_2y = fdr.DataReader('FRED:T10Y2Y')
 t10 = fdr.DataReader('FRED:DGS10')
 #tk10 = fdr.DataReader('KR10YT=RR')
+
 ##
 '''
 import matplotlib.pyplot as plt
@@ -20,29 +21,34 @@ t10y_2y.plot(y='T10Y2Y', color='purple', label='10-Year Treasury to 2-Year Treas
 t10.plot(y='DGS10', color='orange', label='10 year Treasury', title='10Y Yield')
 plt.show()
 '''
+
+
 ##
+start_date = '1996-12-13'
 def normalize(df):
     df = (df - df.min()) / (df.max() - df.min())
     return df
 
-def stock_diff(df):
-    df = df.drop(['Volume'], axis=1).dropna()
-    df = df.pct_change()
-    df = normalize(df)
-    df = df.apply(lambda x: 2 * x - 1)
-    return df
+def stock_diff(df, period=1, day = '1996-12-13'):
+    df = df.dropna()
+    stock_diff = df.pct_change(period)
+    # Normalize
+    volume_norm = normalize(df['Volume'])
+    stock_diff['Volume'] = volume_norm
+    return stock_diff[stock_diff.index>=day]
 
-##
-import matplotlib.pyplot as plt
+hsi_1 = stock_diff(hsi, 1, start_date)
+kospi_1 = stock_diff(kospi, 1, start_date)
+hsi_5 = stock_diff(hsi, 5, start_date)
+kospi_5 = stock_diff(kospi, 5, start_date)
+t10_ = normalize(t10)[t10.index>=start_date]
+krw_usd_ = (krw_usd/krw_usd.max())[krw_usd.index>=start_date]
+t10y_2y_ = (t10y_2y/t10y_2y.max())[t10y_2y.index>=start_date]
 
-hsi_diff = stock_diff(hsi)
-kospi_diff = stock_diff(kospi)
-t10_ = normalize(t10)
-krw_usd_ = krw_usd/krw_usd.max()
-t10y_2y_ = t10y_2y/t10y_2y.max()
-
-ax = hsi_diff.plot(y='Open', color='red', label='HSI', alpha=0.5)
-kospi_diff.plot(y='Open', color='blue', label='KOSPI', alpha=0.5, ax=ax)
+ax = hsi_1.plot(y='Open', color='red', label='HSI', alpha=0.5)
+hsi_1.plot(y='Volume', color='pink', label='HSI volume', alpha=0.5, ax=ax)
+kospi_1.plot(y='Open', color='blue', label='KOSPI', alpha=0.5, ax=ax)
+kospi_1.plot(y='Volume', color='skyblue', label='KOSPI volume', alpha=0.5, ax=ax)
 plt.title('Stock Indices')
 plt.legend()
 plt.show()
@@ -57,6 +63,8 @@ plt.show()
 
 ##
 
-
+ax1 = plt.hist([kospi_5.Open],range=[-0.1, 0.1], bins=1000,alpha=0.5)
+ax2 = plt.hist([hsi_5.Open],range=[-0.1, 0.1], bins=1000,alpha=0.5)
+plt.grid()
+plt.show()
 ##
-
