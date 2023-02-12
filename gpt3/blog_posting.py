1dc92f3dcdc72dc10a2779e6862f0316522b7184
CapacitorA10 <53959052+CapacitorA10@users.noreply.github.com>
2023-02-13 01:16:28 +0900

    Update blog_posting.py

--- gpt3/blog_posting.py
+++ gpt3/blog_posting.py
@@ -4,10 +4,12 @@ import pandas as pd
 import torch
 import torch.nn as nn
 import torch.utils.data as data
+device = "cuda" if torch.cuda.is_available() else "cpu"
+#device = 'cpu'
 
-training_span = 30
+training_span = 32
 cum_volatility = 5
-batch_size = 4
+batch_size = 8
 # tk10 = fdr.DataReader('KR10YT=RR')
 hsi = fdr.DataReader('HSI')
 kospi = fdr.DataReader('KS11')
@@ -126,52 +128,106 @@ train_ = CustomDataset(train)
 test_ = CustomDataset(test)
 # Create a DataLoader with shuffle=True, but only shuffle the first dimension
 train_dataloader = torch.utils.data.DataLoader(train_, batch_size=batch_size, shuffle=True, drop_last=True)
-test_dataloader =  torch.utils.data.DataLoader(train_, batch_size=1, shuffle=False)
+test_dataloader =  torch.utils.data.DataLoader(test_, batch_size=1, shuffle=False)
 
 ## 신경망 수립
+class ResidualBlock(nn.Module):
+    def __init__(self, input_features, hidden_features, kernel_size, dilation_rate, dropout=0.2):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.utils.weight_norm(nn.Conv1d(input_features, hidden_features, kernel_size,
+                               dilation=dilation_rate,
+                               padding=(kernel_size-1)*dilation_rate,
+                               bias=False))
+        self.conv2 = nn.utils.weight_norm(nn.Conv1d(hidden_features, hidden_features, kernel_size,
+                               dilation=dilation_rate,
+                               padding=(kernel_size-1)*dilation_rate,
+                               bias=False))
+        self.dropout = nn.Dropout(dropout)
+        if input_features != hidden_features:
+            self.residual_conv = nn.Conv1d(input_features, hidden_features, 1)
 
+    def forward(self, x):
+        residual = x
+        x = self.conv1(x)
+        x = x[:, :, :-(self.conv1.dilation[0] * (self.conv1.kernel_size[0] - 1))]
+        x = self.dropout(torch.relu(x))
+        x = self.conv2(x)
+        x = x[:, :, :-(self.conv2.dilation[0] * (self.conv2.kernel_size[0] - 1))]
+        x = self.dropout(torch.relu(x))
+        if hasattr(self, 'residual_conv'):
+            residual = self.residual_conv(residual)
+        x = x + residual
+        return x
 
 class TCN(nn.Module):
     def __init__(self, input_features, hidden_features, kernel_size, dilation_rates, num_layers, dropout=0.2):
         super(TCN, self).__init__()
         self.dilation_rates = dilation_rates
         self.num_layers = num_layers
+        self.residual_blocks = nn.ModuleList()
+        for i in range(self.num_layers):
+            self.residual_blocks.append(ResidualBlock(input_features if i == 0 else hidden_features,
+                                                      hidden_features,
+                                                      kernel_size,
+                                                      dilation_rates[i],
+                                                      dropout))
+        self.fc = nn.Linear(hidden_features, 1)
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1)
+        for i, residual_block in enumerate(self.residual_blocks):
+            x = residual_block(x)
+        x = x.permute(0, 2, 1)
+        x = self.fc(x[:, -1, :])
+        return x
+
+class TCN_vanilla(nn.Module):
+    def __init__(self, input_features, hidden_features, kernel_size, dilation_rates, num_layers, dropout=0.2):
+        super(TCN_vanilla, self).__init__()
+        self.dilation_rates = dilation_rates
+        self.num_layers = num_layers
         self.convs = nn.ModuleList()
         self.dropout = nn.Dropout(dropout)
-        self.convs.append(nn.Conv1d(input_features, hidden_features, kernel_size, dilation=dilation_rates[0]))
+        self.convs.append(nn.Conv1d(input_features, hidden_features, kernel_size,
+                                   dilation=dilation_rates[0],
+                                   padding=(kernel_size-1)*dilation_rates[0],
+                                   bias=False))
         for i in range(1, self.num_layers):
-            self.convs.append(nn.Conv1d(hidden_features, hidden_features, kernel_size, dilation=dilation_rates[i]))
+            self.convs.append(nn.Conv1d(hidden_features, hidden_features, kernel_size,
+                                        dilation=dilation_rates[i],
+                                        padding=(kernel_size-1)*dilation_rates[i],
+                                        bias=False))
         self.fc = nn.Linear(hidden_features, 1)
 
     def forward(self, x):
         x = x.permute(0, 2, 1)
         for i, conv in enumerate(self.convs):
-            x = self.dropout(torch.relu(conv(x)))
-            #print(x.shape)
+            x = conv(x)
+            x = self.dropout(torch.relu(x[:, :, :-(conv.dilation[0] * (conv.kernel_size[0] - 1))]))
         x = x.permute(0, 2, 1)
         x = self.fc(x[:, -1, :])
         return x
-
 input_features = train.shape[-1]-1
 hidden_features = input_features * 4
 kernel_size = 3
-num_layers = 3
+num_layers = 5
 dilation_rates = [2**i for i in range(num_layers)]
-model = TCN(input_features, hidden_features, kernel_size, dilation_rates, num_layers)
 
-# Define the loss function and optimizer
+## Define the loss function and optimizer
+model = TCN(input_features, hidden_features, kernel_size, dilation_rates, num_layers).to(device)
 criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.00001)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 
 # Train the model
-num_epochs = 3
+num_epochs = 5
 loss_ = 0
 loss_cum = []
 for epoch in range(num_epochs):
     iter = 0
     for inputs in train_dataloader:
         # Forward pass
-        targets = inputs[:,-1, 3]
+        inputs = inputs.to(device)
+        targets = inputs[:,-1, 3].to(device)
         outputs = model(inputs[:, :-1, :-1])
         loss = criterion(outputs, targets)
 
@@ -181,18 +237,18 @@ for epoch in range(num_epochs):
         optimizer.step()
         loss_ += loss
         # Print the loss every 100 iter
-        if (iter+1) % 10 == 0:
+        iter += 1
+        if (iter) % 10 == 0:
             print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss_:.3f}')
-
             loss_cum.append(loss_.cpu().detach())
             loss_ = 0
-
-        iter += 1
-
+    loss_ = 0
 
 ##
 import matplotlib.pyplot as plt
-plt.plot(loss_cum)
-
+plt.plot(loss_cum, label='no init weight', color='orange', alpha=0.6)
+plt.legend()
+plt.ylim(0,0.4)
+plt.show()
 ##
 
